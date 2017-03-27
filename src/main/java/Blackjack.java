97757d5fa2624ead17895107e81d9864f3f6c4ca
André Roaldseth <andre.roaldseth@schibsted.com>
2017-03-27 21:38:24 +0200

    Add Blackjack.java

---
+++ src/main/java/Blackjack.java
@@ -0,0 +1,28 @@
+import java.util.Collection;
+import java.util.Optional;
+
+public class Blackjack {
+  private Shuffle shuffle;
+  private Deck deck;
+  private Table table;
+
+  public Blackjack(String shuffleEndpoint) {
+    // TODO: Implement a factory that can change implementation and inject
+    // different implementations
+    this.shuffle = new RemoteShuffle(shuffleEndpoint);
+    this.deck = new Deck();
+
+    Dealer dealer = new Dealer(deck);
+    this.table = new Table(dealer);
+  }
+
+  public void seatPlayers(Collection<Player> players) {
+    players.forEach(player -> this.table.seatPlayer(player));
+  }
+
+  public Result play() {
+    this.shuffle.shuffle(this.deck);
+
+    return table.playRound();
+  }
+}
